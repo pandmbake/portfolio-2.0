0e0e88e9f27bcb242b524f3fb973fdb6bd21747d
Paul Bakeman <pandmbake@comcast.net>
2022-07-25 10:33:48 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("headshot");
+var y = document.getElementById("color-shot");
+var b = document.getElementById('banner-middle')
+var d = document.getElementById('banner-middle-add');
+
+function change() {
+    b.style.display = "block";
+    
+}
