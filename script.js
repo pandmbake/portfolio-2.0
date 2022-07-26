7f02bbe84b9fd9c1856c3c49818e777ae3906632
Paul Bakeman <pandmbake@comcast.net>
2022-07-26 18:10:39 -0400

    fixes

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var x = document.getElementById("headshot");
-var y = document.getElementById("color-shot");
 var b = document.getElementById('banner-middle')
 var d = document.getElementById('banner-middle-add');
 
 function change() {
     b.style.display = "block";
-    x.style.width ="30em";
+    
+    
     
 }
